3db597c3c66ebdd14d5d2baed116468d6674384f
Jannik-Hm <jannik.heym@gmx.de>
2026-04-06 21:13:14 +0200

    added environment config helper

---
+++ internal/config/config.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	DefaultGatusImage string
+}
+
+func Load() (*Config, error) {
+	result := Config{}
+
+	default_image, exists := os.LookupEnv("DEFAULT_GATUS_IMAGE")
+	if !exists {
+		return nil, fmt.Errorf("Missing env var for default gatus image (key DEFAULT_GATUS_IMAGE)")
+	}
+	result.DefaultGatusImage = default_image
+
+	return &result, nil
+}
