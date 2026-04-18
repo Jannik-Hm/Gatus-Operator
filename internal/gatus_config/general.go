d898d71027f64a4c1ed49cf7c1da24ce01236b59
Jannik-Hm <jannik.heym@gmx.de>
2026-04-18 21:33:20 +0200

    added helper funcs for config merging

--- internal/gatus_config/general.go
+++ internal/gatus_config/general.go
@@ -28,9 +28,35 @@ type Merger[T any] interface {
 	Merge(...T) T
 }
 
-func FillIfNotValue[T comparable](existing T, fallback T, zero T) T {
-	if existing == zero {
+func FillIfValue[T comparable](existing T, fallback T, value T) T {
+	if existing == value {
 		return fallback
 	}
 	return existing
+}
+
+func MergeIntoList[T any](existing []T, fallback []T) {
+	for _, element := range fallback {
+		existing = append(existing, element)
+	}
+}
+
+func MergeIntoListUnique[T comparable](existing []T, fallback []T) {
+	existing_values := map[T]struct{}{}
+	for _, value := range existing {
+		existing_values[value] = struct{}{}
+	}
+	for _, value := range fallback {
+		if _, ok := existing_values[value]; !ok {
+			existing = append(existing, value)
+		}
+	}
+}
+
+func MergeIntoMap[Key comparable, Value any](existing map[Key]Value, fallback map[Key]Value) {
+	for key, value := range fallback {
+		if _, ok := existing[key]; !ok {
+			existing[key] = value
+		}
+	}
 }
