f7c06c5b9d45e3320c9152367a59ad0a8d5fb6b7
Jannik-Hm <jannik.heym@gmx.de>
2026-04-26 00:11:03 +0200

    made watch flags configurable via env vars

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 type Config struct {
@@ -30,11 +31,48 @@ func Load() (*Config, error) {
 	// TODO: make this user editable
 	result.ProtocolPreference = []string{"https", "http", "tcp", "udp"}
 
-	// TODO: make this user editable
+	// defaults
 	result.WatchIngresses = true
 	result.WatchIngressClasses = true
 	result.WatchHTTPRoutes = true
 	result.WatchGateways = true
+
+	// get env settings
+	watch_ingress, exists := os.LookupEnv("WATCH_INGRESS")
+	if exists {
+		val, err := strconv.ParseBool(watch_ingress)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing WATCH_INGRESS: %w", err)
+		}
+		result.WatchIngresses = val
+	}
+
+	watch_ingress_class, exists := os.LookupEnv("WATCH_INGRESSCLASS")
+	if exists {
+		val, err := strconv.ParseBool(watch_ingress_class)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing WATCH_INGRESSCLASS: %w", err)
+		}
+		result.WatchIngressClasses = val
+	}
+
+	watch_http_route, exists := os.LookupEnv("WATCH_HTTPROUTE")
+	if exists {
+		val, err := strconv.ParseBool(watch_http_route)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing WATCH_HTTPROUTE: %w", err)
+		}
+		result.WatchHTTPRoutes = val
+	}
+
+	watch_gateway, exists := os.LookupEnv("WATCH_GATEWAY")
+	if exists {
+		val, err := strconv.ParseBool(watch_gateway)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing WATCH_GATEWAY: %w", err)
+		}
+		result.WatchGateways = val
+	}
 
 	return &result, nil
 }
